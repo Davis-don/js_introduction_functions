e2dbbfe36a4da302764fdfb93383f944bd25983e
Davis_the_don <davismugoikou@gmail.com>
2024-09-13 23:36:40 +0300

    function increments value passed

---
+++ nextNumber.js
@@ -0,0 +1,7 @@
+//function to return next number from integer passed
+
+const nextNumberFromIntegerPassed=(integer)=>{
+  return ++integer;
+}
+//implementation
+console.log(nextNumberFromIntegerPassed(-3))
