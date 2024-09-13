eac22dc5c5cbd79cd770714cc5cb8630e0c2359d
Davis_the_don <davismugoikou@gmail.com>
2024-09-13 23:35:08 +0300

    converts minutes to seconds

---
+++ minutesToSecondsConvertor.js
@@ -0,0 +1,7 @@
+//function to convert minutes into seconds
+const convertMinutesToSeconds=(minutes)=>{
+ let secondsConverted=minutes * 60;
+ return secondsConverted;
+}
+//Implementation
+console.log(convertMinutesToSeconds(3));
